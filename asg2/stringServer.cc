a6af94ac5683928484f3dd87087022b0c7d18884
Rosie Ye <rosieye1992@gmail.com>
2015-02-22 00:48:28 -0500

    stringServer add multiclient, 但是目前还在调

--- asg2/stringServer.cc
+++ asg2/stringServer.cc
@@ -10,31 +10,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 
-#define NUM_THREADS	5
+#define NUM_CLIENTS 2
 
-void *PrintHello(void *threadid)
+
+int client_no = 0;
+struct sockaddr_in serv_addr, cli_addr;
+
+void *rw_socket(void *sock_ptr)
 {
-	long tid;
-	tid = (long)threadid;
-	printf("Hello World! It's me, thread #%ld!\n", tid);
-	pthread_exit(NULL);
+	long newsock = (long)sock_ptr;
+	int newsockfd = (int)newsock;
+     char buffer[256];
+	 int n;
+	 while(true)
+	 {
+		 bzero(buffer,256);
+		 n = read(newsockfd,buffer,255);
+		 if(n <2)
+			 break;
+		 if (n < 0) puts("ERROR reading from socket");
+		 printf("Here is the message: %s\n",buffer);
+		 n = write(newsockfd,buffer,sizeof(buffer));
+		 if (n < 0) puts("ERROR writing to socket");
+	 }
+	 close(newsockfd);
+	 pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	pthread_t threads[NUM_THREADS];
 	int rc;
 	long t;
-	for(t=0;t<NUM_THREADS;t++){
-		printf("In main: creating thread %ld\n", t);
-		rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+	int sockfd, portno;
+	socklen_t clilen;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) 
+		puts("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = 8080;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+				sizeof(serv_addr)) < 0) 
+		puts("ERROR on binding");
+	listen(sockfd, NUM_CLIENTS);
+	while(true)
+	{
+		clilen = sizeof(cli_addr);
+		long newsockfd = accept(sockfd, 
+				(struct sockaddr *) &cli_addr, 
+				&clilen);
+		if (newsockfd < 0) 
+			puts("ERROR on accept");
+		pthread_t threads;
+		rc = pthread_create(&threads, NULL, rw_socket, (void *)newsockfd);
 		if (rc){
 			printf("ERROR; return code from pthread_create() is %d\n", rc);
 			exit(-1);
 		}
 	}
-
+	close(sockfd);
 	/* Last thing that main() should do */
 	pthread_exit(NULL);
 }
