a0660c480963561ac1bf033637b534fabf88fedf
Rosie Ye <rosieye1992@gmail.com>
2015-02-22 23:41:16 -0500

    almost done; select added

--- asg2/stringClient.cc
+++ asg2/stringClient.cc
@@ -12,16 +12,22 @@
 #include <vector>
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
 using namespace std;
 
 vector<string> buff;
+int sockfd;
 
 void *ReadString(void *threadid)
 {
 	long tid;
 	tid = (long)threadid;
-	printf("Hello World! It's me, thread #%ld!\n", tid);
+//	printf("Hello World! It's me, thread #%ld!\n", tid);
 	while(true)
 	{
 		string st;
@@ -34,8 +40,11 @@ void *ReadString(void *threadid)
 void *SendString(void *threadid)
 {
 	long tid;
+	char padding[1];
+	char buffer[256];
+	uint8_t content_len;
 	tid = (long)threadid;
-	printf("Hello World! It's me, thread #%ld!\n", tid);
+//	printf("Hello World! It's me, thread #%ld!\n", tid);
 	while(true)
 	{
 		if(buff.empty())
@@ -43,6 +52,50 @@ void *SendString(void *threadid)
 		string st = buff.front();
 		buff.erase(buff.begin());
 		cout<<"puts"<<st<<endl;
+		bzero(buffer, sizeof(buffer));
+		strcpy(buffer, st.c_str());
+		content_len = (uint8_t)strlen(buffer);
+		
+		// Send string length
+		if(send(sockfd, (char *)(&content_len), 1, 0) < 0) {
+			perror("Error sending content length");
+			return NULL;
+		}
+		
+		// Send string
+		if(send(sockfd, buffer, (ssize_t)content_len, 0) < 0) {
+			perror("ERROR writing to socket");
+			return NULL;
+		}
+
+		// send padding zero
+		if(send(sockfd, "\0", 1, 0) < 0) {
+			perror("ERROR writing to socket");
+			return NULL;
+		}
+
+		// recv reply msg length
+		if (recv(sockfd, buffer, 1, 0) < 0) {
+			perror("ERROR reading from socket");
+			return NULL;
+		}
+		ssize_t reply_len = (ssize_t)buffer[0];
+		
+		bzero(buffer, sizeof(buffer));
+		// recv reply msg
+		if (recv(sockfd, buffer, reply_len, MSG_WAITALL) < 0) {
+			perror("ERROR reading from socket");
+			return NULL;
+		}
+		// recv ending zero
+		if (recv(sockfd, padding, sizeof(padding), 0) < 0) {
+			perror("ERROR reading from socket");
+			return NULL;
+		}
+		if (padding[0] != '\0') {
+			printf("Error: reply msg should end with '\\0'\n");
+		}
+		printf("[%s]\n", buffer);
 		sleep(5);
 	}
 	pthread_exit(NULL);
@@ -50,6 +103,36 @@ void *SendString(void *threadid)
 
 int main(int argc, char *argv[])
 {
+    int portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+	uint8_t content_len;
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    
+	portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        perror("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+	int newsockfd = connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr));
+	if(newsockfd<0)
+		perror("ERROR connecting");
+	/*
+	 * open thread section
+	 */
 	pthread_t read_th, sock_th;
 	int rc;
 	long t;
@@ -64,7 +147,6 @@ int main(int argc, char *argv[])
 			exit(-1);
 		}
 	}
-
 	/* Last thing that main() should do */
 	pthread_exit(NULL);
 }
--- asg2/stringServer.cc
+++ asg2/stringServer.cc
@@ -14,74 +14,139 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <set>
 
 #define NUM_CLIENTS 2
 
+using namespace std;
 
-int client_no = 0;
+
 struct sockaddr_in serv_addr, cli_addr;
+fd_set rfds;
+set<int> rfds_set;
+int max_fd;
 
-void *rw_socket(void *sock_ptr)
+
+void rw_socket(int newsockfd)
 {
-	int newsockfd = (int)((long)sock_ptr);
+	//	int newsockfd = (int)((long)sock_ptr);
 	char buffer[256];
 	char reply_msg[256];
 	char padding[1];
 	int n;
 	uint8_t content_len;
+	bzero(buffer, sizeof(buffer));
+	bzero(reply_msg, sizeof(buffer));
+	// recv reply msg length
+	n = recv(newsockfd, buffer, 1, 0);
+	if (n < 0) {
+		perror("ERROR reading from socket");
+		rfds_set.erase(newsockfd);
+		return;
+	}
+	if (n == 0) {
+		printf("Client Disconnected\n");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+	ssize_t msg_len = (ssize_t)buffer[0];
+	// recv reply msg
+	if (recv(newsockfd, buffer, msg_len, MSG_WAITALL) < 0) {
+		perror("ERROR reading from socket");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+	printf("msg recv: %s\n", buffer);
+	// recv ending zero
+	if (recv(newsockfd, padding, sizeof(padding), 0) < 0) {
+		perror("ERROR reading from socket");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+	if (padding[0] != '\0') {
+		printf("Error: reply msg should end with '\\0'\n");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+
+	strncpy(reply_msg, buffer, msg_len);
+	content_len = (uint8_t)strlen(reply_msg);
+
+	// Send string length
+	if(send(newsockfd, (char *)(&content_len), 1, 0) < 0) {
+		perror("Error sending content length");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+
+	// Send string
+	if(send(newsockfd, reply_msg, (ssize_t)content_len, 0) < 0) {
+		perror("ERROR writing to socket");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+
+	// send padding zero
+	if(send(newsockfd, "\0", 1, 0) < 0) {
+		perror("ERROR writing to socket");
+		FD_CLR(newsockfd, &rfds);
+		rfds_set.erase(newsockfd);
+		return;
+	}
+	return;
+}
+
+void init_rfds(void)
+{
+	set<int>::iterator it;
+	for(it = rfds_set.begin(); it != rfds_set.end(); it++)
+	{
+		printf("%d\n", *it);
+		FD_SET(*it, &rfds);
+	}
+}
+
+void *select_client(void *sockfd_ptr)
+{
+	struct timeval timeout;
+
+	/* Set time limit. */
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
 	while(true)
 	{
-		bzero(buffer, sizeof(buffer));
-		bzero(reply_msg, sizeof(buffer));
-		// recv reply msg length
-		n = recv(newsockfd, buffer, 1, 0);
-		if (n < 0) {
-			perror("ERROR reading from socket");
-			break;
-		}
-		if (n == 0) {
-			printf("Client Disconnected\n");
-			break;
-		}
-		ssize_t msg_len = (ssize_t)buffer[0];
-		// recv reply msg
-		if (recv(newsockfd, buffer, msg_len, MSG_WAITALL) < 0) {
-			perror("ERROR reading from socket");
-			break;
-		}
-		// recv ending zero
-		if (recv(newsockfd, padding, sizeof(padding), 0) < 0) {
-			perror("ERROR reading from socket");
-			break;
-		}
-		if (padding[0] != '\0') {
-			printf("Error: reply msg should end with '\\0'\n");
-			break;
-		}
-		
-		strncpy(reply_msg, buffer, msg_len);
-		content_len = (uint8_t)strlen(reply_msg);
-		
-		// Send string length
-		if(send(newsockfd, (char *)(&content_len), 1, 0) < 0) {
-			perror("Error sending content length");
-			break;
-		}
-		
-		// Send string
-		if(send(newsockfd, reply_msg, (ssize_t)content_len, 0) < 0) {
-			perror("ERROR writing to socket");
-			break;
-		}
+		init_rfds();
+		int retval = select(max_fd+1, &rfds, NULL, NULL, &timeout);
+		timeout.tv_sec = 1;
+		timeout.tv_usec = 0;
 
-		// send padding zero
-		if(send(newsockfd, "\0", 1, 0) < 0) {
-			perror("ERROR writing to socket");
-			break;
+		if (retval == -1)
+			perror("select() error");
+		else if (retval)
+		{
+			printf("Data is available now.\n");
+			set<int>::iterator it;
+//			printf("set size:%d\n", rfds_set.size());
+			for(it = rfds_set.begin(); it != rfds_set.end(); it++)
+			{
+				if(FD_ISSET(*it, &rfds))
+				{
+					rw_socket(*it);
+				}
+			}
 		}
-
+		else
+			printf("No data within %ld seconds.\n", timeout.tv_sec);
+		FD_ZERO(&rfds);
 	}
-	close(newsockfd);
 	pthread_exit(NULL);
 }
 
@@ -107,21 +172,30 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	listen(sockfd, NUM_CLIENTS);
+	max_fd = sockfd;
+	/*
+	 * set file descriptor set
+	 */
+
+	FD_ZERO(&rfds);
+	pthread_t threads_select;
+	rc = pthread_create(&threads_select, NULL, select_client, (void *) (long)sockfd);
+	if (rc) {
+		printf("ERROR; return code from pthread_create() is %d\n", rc);
+		exit(-1);
+	}
 	while(true)
 	{
 		clilen = sizeof(cli_addr);
-		long newsockfd = accept(sockfd, 
+		int newsockfd = accept(sockfd, 
 				(struct sockaddr *) &cli_addr, 
 				&clilen);
 		if (newsockfd < 0) {
 			perror("ERROR on accept");
 		}
-		pthread_t threads;
-		rc = pthread_create(&threads, NULL, rw_socket, (void *)newsockfd);
-		if (rc) {
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
+		max_fd = max(max_fd, newsockfd);
+		printf("new sockfd:%d\n", newsockfd);
+		rfds_set.insert(newsockfd);
 	}
 	close(sockfd);
 	/* Last thing that main() should do */
